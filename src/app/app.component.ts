a0b3e27f2125f6c0a86684927e3a5259c3d40ec3
Paulo Penteado <ppenteado@gdginc.local>
2019-06-07 16:33:57 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'salsa-qc';
+  title = 'Salsa Attitude';
+  home = 'Accueil';
+  who_we_are = "L'école";
+  news = 'Nouvelles';
+  courses_n_schedule = 'Cours et horaire';
+  subscription = 'Inscription';
+  events = 'Événements';
+  contact = 'Contact';
+  search = 'Chercher';
 }
