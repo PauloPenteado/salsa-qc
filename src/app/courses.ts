67241cfaed0d714cc798e736f88edca9ee479c6b
Paulo Penteado <ppenteado@gdginc.local>
2019-06-14 15:04:02 -0400

    Minor change

--- src/app/courses.ts
+++ src/app/courses.ts
@@ -2,56 +2,48 @@ export const courses = [
     {
         name: 'Bachata Dominicaine',
         price: 50,
-        descriptionEN: '',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         disponible: 'y'
     },
     {
         name: 'Bachata Sensuelle',
         price: 70,
-        descriptionEN: '',
         description: 'Lorem ipsum',
         disponible: 'y'
     },
     {
         name: 'Kizomba',
         price: 50,
-        descriptionEN: '',
         description: '',
         disponible: 'y'
     },
     {
         name: 'Lady Styling Bachara Choréo',
         price: 50,
-        descriptionEN: '',
         description: '',
         disponible: 'y'
     },
     {
         name: 'Salsa',
         price: 50,
-        descriptionEN: '',
         description: '',
         disponible: 'y'
     },
     {
         name: 'Salsa Cubaine',
         price: 50,
-        descriptionEN: '',
         description: '',
         disponible: 'y'
     },
     {
         name: 'Salsa Lady Styling',
         price: 50,
-        descriptionEN: '',
         description: '',
         disponible: 'y'
     },
     {
         name: 'Salsa Men Styling',
         price: 50,
-        descriptionEN: '',
         description: '',
         disponible: 'y'
     },
