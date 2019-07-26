8dace0e345168dde2b88f88a7ce55f3deb20602e
Paulo Penteado <ppenteado@gdginc.local>
2019-07-26 15:53:20 -0400

    Improving code readability and organization

---
+++ src/app/plans.ts
@@ -0,0 +1,34 @@
+export const plans = [
+    {
+        name: 'Débutant 1',
+        price: 50,
+        detail_1: '1 cours',
+        detail_2: 'Niveau Débutant',
+        detail_3: 'Pas à Pas',
+        disponible: 'y'
+    },
+    {
+        name: 'Débutant 2 ou plus',
+        price: 80,
+        detail_1: '1 cours',
+        detail_2: 'Niveau Débutant 2 ou plus',
+        detail_3: 'Pas à Pas',
+        disponible: 'y'
+    },
+    {
+        name: 'Débutant ILLIMITÉ',
+        price: 75,
+        detail_1: 'Accès limité pour TOUS les cours niveau débutant 1',
+        detail_2: '',
+        detail_3: '',
+        disponible: 'y'
+    },
+    {
+        name: 'ILLIMITÉ TOTAL',
+        price: 125,
+        detail_1: 'TOUS les cours',
+        detail_2: 'N importe quel niveau',
+        detail_3: '',
+        disponible: 'y'
+    },        
+]
--- src/app/subscription/subscription.component.ts
+++ src/app/subscription/subscription.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { plans } from "../plans"
 
 @Component({
   selector: 'app-subscription',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subscription.component.css']
 })
 export class SubscriptionComponent implements OnInit {
+
+  plans = plans;
 
   constructor() { }
 
