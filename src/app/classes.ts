24d60f7de58ab56e5508813de523eb68d881ce62
Paulo Penteado <ppenteado@gdginc.local>
2019-07-30 11:56:44 -0400

    Add schedule table

---
+++ src/app/classes.ts
@@ -0,0 +1,106 @@
+export const classes = [
+    {
+        name: 'Bachata Dominicaine',
+        instructor: '',
+        description: '',
+        schedule: {
+            dimanche: '',
+            lundi: '20-21h',
+            mardi: '',
+            mercredi: '',
+            jeudi: '19h-20h',
+            vendredi: '',
+        }
+    },
+    {
+        name: 'Bachata Sensuelle',
+        instructor: '',
+        description: '',
+        schedule: {
+            dimanche: '',
+            lundi: '',
+            mardi: '',
+            mercredi: '19h-20h',
+            jeudi: '',
+            vendredi: '',
+        }
+    },
+    {
+        name: 'Kizomba',
+        instructor: '',
+        description: '',
+        schedule: {
+            dimanche: '19h-20h',
+            lundi: '',
+            mardi: '20-21h',
+            mercredi: '',
+            jeudi: '19h-20h',
+            vendredi: '',
+        }
+    },
+    {
+        name: 'Lady Styling Bachara Choréo',
+        instructor: '',
+        description: '',
+        schedule: {
+            dimanche: '19h-20h',
+            lundi: '20-21h',
+            mardi: '',
+            mercredi: '',
+            jeudi: '19h-20h',
+            vendredi: '',
+        }
+    },
+    {
+        name: 'Salsa Débutant 1',
+        instructor: '',
+        description: '',
+        schedule: {
+            dimanche: '',
+            lundi: '',
+            mardi: '20-21h',
+            mercredi: '19h-20h',
+            jeudi: '',
+            vendredi: '',
+        }
+    },
+    {
+        name: 'Salsa Cubaine',
+        instructor: '',
+        description: '',
+        schedule: {
+            dimanche: '',
+            lundi: '19h-20h',
+            mardi: '',
+            mercredi: '',
+            jeudi: '',
+            vendredi: '',
+        }
+    },
+    {
+        name: 'Salsa Lady Styling',
+        instructor: '',
+        description: '',
+        schedule: {
+            dimanche: '',
+            lundi: '',
+            mardi: '',
+            mercredi: '',
+            jeudi: '19h-20h',
+            vendredi: '',
+        }
+    },
+    {
+        name: 'Salsa Men Styling',
+        instructor: '',
+        description: '',
+        schedule: {
+            dimanche: '',
+            lundi: '',
+            mardi: '',
+            mercredi: '19h-20h',
+            jeudi: '',
+            vendredi: '',
+        }
+    },
+]
--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { classes } from "../classes";
 
 @Component({
   selector: 'app-schedule',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
+
+  classes = classes;
 
   constructor() { }
 
